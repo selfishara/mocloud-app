a9e7e219dc1d8378470b881d7942f8c6851edb32
sara <saramb10.smb@alumnes.ilerna.com>
2026-05-23 14:11:20 +0200

    Initial KMP project scaffold

---
+++ shared/src/commonMain/kotlin/com/mocloud/app/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.mocloud.app
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/commonMain/kotlin/com/mocloud/app/Platform.kt
@@ -0,0 +1,7 @@
+package com.mocloud.app
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
